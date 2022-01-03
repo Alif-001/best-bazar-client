0ccf99ec79c3cfae6adf4c4151045daeb47691a4
Suhag Al Amin <developersuhag@gmail.com>
2022-01-03 10:22:48 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
 import Home from "./pages/Home/Home/Home";
+import Login from "./pages/Login/Login/Login";
+import Registration from "./pages/Login/Registration/Registration";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import global from "./styles/global.scss";
 
 function App() {
   return (
-    <Router>
-      <>
-        <Home />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
         <ToastContainer
           position="top-center"
           autoClose={3000}
@@ -22,8 +31,8 @@ function App() {
           draggable
           pauseOnHover
         />
-      </>
-    </Router>
+      </Router>
+    </>
   );
 }
 
