f6c5a3421c8f7b17d83fe6cb2a6602f06ba923f7
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2022-01-03 22:59:39 +0600

    slier error fixed

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -1,4 +1,10 @@
 const firebaseConfig = {
+  // apiKey: "AIzaSyDInJIJw1C5kDHFlKQHZLTCY5ekJMN-U2Y",
+  // authDomain: "fir-test-f43b3.firebaseapp.com",
+  // projectId: "fir-test-f43b3",
+  // storageBucket: "fir-test-f43b3.appspot.com",
+  // messagingSenderId: "272734522738",
+  // appId: "1:272734522738:web:58caeaaaccffddcd79a1eb",
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -1,44 +1,54 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
+import "./Slider.css";
 
 const Slider = () => {
   return (
-    <Carousel fade className="h-100">
+    <Carousel fade controls={false} indicators={false} interval={1500}>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          id="hero"
+          className="d-block w-100 hero-img"
           src="https://i.ibb.co/mvVj4T5/700302-generic-desktop-radeon-6700xt-transparent-1260x709.webp?fbclid=IwAR1W6qUqg0pnL-LWW_fKRK-5XVV25SmMo1HxYvzBnVP6IFrpI87RkeCdB7o"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h1>Laptop & Desktop </h1>
+          <h3>SAVE UP TO 40%</h3>
+          <p>
+            Our Assortment Of laptop & Desktop Are On Sale Now For Limited Time!
+          </p>
+          <Button className="shop-btn">Shop Now</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100  hero-img"
           src="https://i.ibb.co/JK1t3PC/b320e438631991-5769476692396.jpg?fbclid=IwAR3yA9mqrp_jJgpJx2u3rh1CDRED8oiCcSFh2IbZamQKZzB_8NOyrSsMx7Y"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1>Luggage </h1>
+          <h3>SAVE UP TO 60%</h3>
+          <p>Our Assortment Of Luggage Are On Sale Now For Limited Time!</p>
+          <Button className="shop-btn">Shop Now</Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block w-100  hero-img"
           src="https://i.ibb.co/tLgbyvV/maxresdefault.jpg?fbclid=IwAR073buGlwx7LbYzPtoxh11DZNaYriShCqQCTAuJAiejx6xdaZZqTkJP7gM"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h1>Laptop & Desktop </h1>
+          <h3>SAVE UP TO 40%</h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Our Assortment Of laptop & Desktop Are On Sale Now For Limited Time!
           </p>
+          <Button className="shop-btn">Shop Now</Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
