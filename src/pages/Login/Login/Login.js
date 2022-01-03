9d9a10318b2ff01f463ee0f41707670f1ccac039
Suhag Al Amin <developersuhag@gmail.com>
2022-01-03 11:11:43 +0600

    authentication

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,10 +1,64 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import registerStyles from "../../../styles/Register.module.scss";
 
 const Login = () => {
+  const { signInWithGoogle, handleEmailLogin } = useAuth();
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { email, password } = data;
+    handleEmailLogin(email, password);
+  };
+  console.log(errors);
   return (
-    <div>
-      <h3>this is login</h3>
-    </div>
+    <>
+      <Container className="py-5 mb-5">
+        <h3 className={`${registerStyles.title} text-center mb-4`}>
+          Please Login
+        </h3>
+        <div className={`${registerStyles.registration} shadow`}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Control
+              type="text"
+              placeholder="Email Address"
+              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            <Form.Control
+              type="password"
+              placeholder="Your password"
+              {...register("password", { required: true })}
+            />
+
+            <Button type="submit" variant="success">
+              Login
+            </Button>
+          </form>
+          <p>
+            New user? <Link to="/register">Register here</Link>{" "}
+          </p>
+          <div>
+            <p className="text-center border-bottom pb-1">
+              Or sign in Using Google
+            </p>
+            <div className="text-center">
+              <Button onClick={signInWithGoogle} variant="text">
+                {" "}
+                <FcGoogle className="fs-1" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 };
 
--- src/pages/Login/Registration/Registration.js
+++ src/pages/Login/Registration/Registration.js
@@ -1,76 +1,80 @@
-// import React from "react";
-// import { Container } from "react-bootstrap";
-// import { useForm } from "react-hook-form";
-// import { toast } from "react-toastify";
+import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import useAuth from "../../../hooks/useAuth";
+import registerStyles from "../../../styles/Register.module.scss";
 
-// const Registration = () => {
-//     // const { handleEmailRegister, signInWithGoogle } = useAuth();
+const Registration = () => {
+  const { handleEmailRegister, signInWithGoogle } = useAuth();
 
-//     const {
-//       register,
-//       handleSubmit,
-//       formState: { errors },
-//     } = useForm();
-//     const onSubmit = (data) => {
-//       const { userName, email, password, reEnterPassword } = data;
-//       if (password !== reEnterPassword) {
-//         toast.info("Password Is not matched");
-//         return;
-//       }
-//       handleEmailRegister(userName, email, password);
-//     };
-//     console.log(errors);
-//     return (
-//       <>
-//         <Container className="py-5 mb-5">
-//           <h3 className={`${registerStyles.title} text-center mb-4`}>
-//             Registration
-//           </h3>
-//           <div className={`${registerStyles.registration} shadow`}>
-//             <form onSubmit={handleSubmit(onSubmit)}>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="Your Name"
-//                 {...register("userName", { required: true })}
-//               />
-//               <Form.Control
-//                 type="email"
-//                 placeholder="Email Address"
-//                 {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-//               />
-//               <Form.Control
-//                 type="password"
-//                 placeholder="Your password"
-//                 {...register("password", { required: true })}
-//               />
-//               <Form.Control
-//                 type="password"
-//                 placeholder="Re enter your password"
-//                 {...register("reEnterPassword", { required: true })}
-//               />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const { userName, email, password, reEnterPassword } = data;
+    if (password !== reEnterPassword) {
+      toast.info("Password Is not matched");
+      return;
+    }
+    handleEmailRegister(userName, email, password);
+  };
+  console.log(errors);
+  return (
+    <>
+      <Container className="py-5 mb-5">
+        <h3 className={`${registerStyles.title} text-center mb-4`}>
+          Registration
+        </h3>
+        <div className={`${registerStyles.registration} shadow`}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Control
+              type="text"
+              placeholder="Your Name"
+              {...register("userName", { required: true })}
+            />
+            <Form.Control
+              type="email"
+              placeholder="Email Address"
+              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            <Form.Control
+              type="password"
+              placeholder="Your password"
+              {...register("password", { required: true })}
+            />
+            <Form.Control
+              type="password"
+              placeholder="Re enter your password"
+              {...register("reEnterPassword", { required: true })}
+            />
 
-//               <Button type="submit" variant="success">
-//                 Register
-//               </Button>
-//             </form>
-//             <p className="mt-2">
-//               Already registered? <Link href="/login">Login</Link>{" "}
-//             </p>
-//             <div>
-//               <p className="text-center border-bottom pb-1">
-//                 Or sign in Using Google
-//               </p>
-//               <div className="text-center">
-//                 <Button onClick={signInWithGoogle} variant="text">
-//                   {" "}
-//                   <FcGoogle className="fs-1" />
-//                 </Button>
-//               </div>
-//             </div>
-//           </div>
-//         </Container>
-//       </>
-//     );
-// };
+            <Button type="submit" variant="success">
+              Register
+            </Button>
+          </form>
+          <p className="mt-2">
+            Already registered? <Link to="/login">Login</Link>{" "}
+          </p>
+          <div>
+            <p className="text-center border-bottom pb-1">
+              Or sign in Using Google
+            </p>
+            <div className="text-center">
+              <Button onClick={signInWithGoogle} variant="text">
+                {" "}
+                <FcGoogle className="fs-1" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-// export default Registration;
+export default Registration;
