7bb5249d00412e199a09f49743ac2dc32d3f0e80
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2022-01-03 20:25:01 +0600

    slider added & footer error fix

--- src/pages/Home/Slider/Slider.js
+++ src/pages/Home/Slider/Slider.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
 const Slider = () => {
-    return (
-        <div>
-            Slider will come here
-        </div>
-    );
+  return (
+    <Carousel fade className="h-100">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/mvVj4T5/700302-generic-desktop-radeon-6700xt-transparent-1260x709.webp?fbclid=IwAR1W6qUqg0pnL-LWW_fKRK-5XVV25SmMo1HxYvzBnVP6IFrpI87RkeCdB7o"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/JK1t3PC/b320e438631991-5769476692396.jpg?fbclid=IwAR3yA9mqrp_jJgpJx2u3rh1CDRED8oiCcSFh2IbZamQKZzB_8NOyrSsMx7Y"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://i.ibb.co/tLgbyvV/maxresdefault.jpg?fbclid=IwAR073buGlwx7LbYzPtoxh11DZNaYriShCqQCTAuJAiejx6xdaZZqTkJP7gM"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 };
 
 export default Slider;
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,32 +1,41 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import logo from "../../../images/logo.png";
 const Footer = () => {
   return (
-    <div>
-      <div className=" d-flex justify-content-center bg-light pt-3">
-        <div className="px-3 ">
-          <h6>Contact us</h6>
-          <ul className="list-unstyled">
-            <li>Email: bestbazer@gmail.com</li>
-            <li>Phone: 01666562666</li>
-            <li>Facebook</li>
-            <li>Instagram</li>
-          </ul>
-        </div>
-        <div className="px-3 ">
-          <img src={logo} alt="footer-logo" />
-        </div>
-        <div className="px-3 ">
-          <h6>WHO WE ARE</h6>
-          <ul className="list-unstyled">
-            <li>About us</li>
-            <li>Reasons to shop</li>
-            <li>Meet the team</li>
-            <li>Cookies & privacy</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <>
+      <Container
+        bg="light"
+        fluid="md"
+        className="pt-4 d-flex justify-content-center"
+      >
+        <Row>
+          <Col xs={12} md="auto">
+            {" "}
+            <h6>Contact us</h6>
+            <ul className="list-unstyled">
+              <li>Email: bestbazer@gmail.com</li>
+              <li>Phone: 01666562666</li>
+              <li>Facebook</li>
+              <li>Instagram</li>
+            </ul>
+          </Col>
+          <Col xs={12} md="auto" className="p-3 text-center">
+            {" "}
+            <img src={logo} alt="footer-logo" />
+          </Col>
+          <Col xs={12} md="auto">
+            <h6>WHO WE ARE</h6>
+            <ul className="list-unstyled">
+              <li>About us</li>
+              <li>Reasons to shop</li>
+              <li>Meet the team</li>
+              <li>Cookies & privacy</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
