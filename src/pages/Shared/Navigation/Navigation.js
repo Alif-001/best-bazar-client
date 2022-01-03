70e7eb3e85fb2b80fb850710b429da24e4a624bb
Suhag Al Amin <developersuhag@gmail.com>
2022-01-03 11:44:24 +0600

    update

--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import './navigation.css'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./navigation.css";
+import logo from "../../../images/logo.png";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Navigation = () => {
-    return (
-        <div>
-            <Navbar bg="light" expand="lg">
-  <Container fluid>
-    <NavLink to="/"> <img src="https://i.ibb.co/tDDnGR9/best-bazar.png" alt="" /> </NavLink>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-    {/* <Form className="d-flex ms-auto">
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <NavLink to="/">
+            {" "}
+            <img src={logo} alt="" />{" "}
+          </NavLink>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            {/* <Form className="d-flex ms-auto">
         <FormControl
           type="search"
           placeholder="Search"
@@ -20,30 +25,32 @@ const Navigation = () => {
         />
         <Button variant="outline-success">Search</Button>
       </Form> */}
-      <Nav
-        className="me-auto my-2 my-lg-0 navLinks"
-        style={{}}
-        navbarScroll
-      >
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/shop">Shop</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </Nav>
-      <Nav
-        className="ms-auto my-2 my-lg-0 navLinks"
-        style={{}}
-        navbarScroll
-      >
-        <NavLink to="/cart"><i class="fas fa-shopping-cart"></i><span>0</span></NavLink>
-        <NavLink to="/login">Login</NavLink>
-      </Nav>
-
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </div>
-    );
+            <Nav
+              className="me-auto my-2 my-lg-0 navLinks"
+              style={{}}
+              navbarScroll
+            >
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/shop">Shop</NavLink>
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/contact">Contact</NavLink>
+            </Nav>
+            <Nav
+              className="ms-auto my-2 my-lg-0 navLinks"
+              style={{}}
+              navbarScroll
+            >
+              <NavLink to="/cart">
+                <FiShoppingCart className="fs-4" />
+                <span>0</span>
+              </NavLink>
+              <NavLink to="/login">Login</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Navigation;
